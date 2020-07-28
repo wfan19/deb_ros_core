58cb00dff69da65af2325f83f558bbc5f8d19156
Bill Fan <1472451850@qq.com>
2020-07-27 23:20:23 -0400

    Serial driver: Added Clearpath's Exar driver for convenience

---
+++ Exar_USB_Serial_Kernel_Driver/.xr_usb_serial_common.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/bf01/Downloads/Teknic-ClearPath-SC-ROS/Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.mod := { echo  /home/bf01/Downloads/Teknic-ClearPath-SC-ROS/Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.o; echo; } > /home/bf01/Downloads/Teknic-ClearPath-SC-ROS/Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.mod
---
+++ Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.mod
@@ -0,0 +1,2 @@
+/home/bf01/Downloads/Teknic-ClearPath-SC-ROS/Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.o
+
