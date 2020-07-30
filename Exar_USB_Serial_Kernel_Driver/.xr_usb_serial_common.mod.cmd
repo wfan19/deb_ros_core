c334c9c89611c2ebbfaf3595c5566a8d3e3eb63f
Bill <billfan.2001@gmail.com>
2020-07-30 04:52:27 +0000

    Git: Untracked build files of serial driver

--- Exar_USB_Serial_Kernel_Driver/.xr_usb_serial_common.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bf01/Downloads/Teknic-ClearPath-SC-ROS/Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.mod := { echo  /home/bf01/Downloads/Teknic-ClearPath-SC-ROS/Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.o; echo; } > /home/bf01/Downloads/Teknic-ClearPath-SC-ROS/Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.mod
--- Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.mod
+++
@@ -1,2 +0,0 @@
-/home/bf01/Downloads/Teknic-ClearPath-SC-ROS/Exar_USB_Serial_Kernel_Driver/xr_usb_serial_common.o
-
