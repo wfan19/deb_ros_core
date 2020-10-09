63d2d89866f50fdc39d757bba15e9796d1fbb9e0
Bill Fan <1472451850@qq.com>
2020-10-09 19:08:42 -0400

    FCS Controller: Added initial class, added configured cmake and package.xml

---
+++ fcs_ros_deb/include/fcs_ros_deb/fcs_ros_deb.hpp
@@ -0,0 +1,10 @@
+//
+// Created by bf01 on 10/9/20.
+//
+
+#ifndef SRC_FCS_ROS_DEB_HPP
+#define SRC_FCS_ROS_DEB_HPP
+
+
+
+#endif //SRC_FCS_ROS_DEB_HPP
---
+++ fcs_ros_deb/src/fcs_ros_deb.cpp
@@ -0,0 +1,13 @@
+//
+// Created by bf01 on 10/9/20.
+//
+
+#include <ros/ros.h>
+
+#include <fcs_ros_deb/fcs_ros_deb.hpp>
+
+int main(int argc, char** argv)
+{
+  ROS_INFO("Hello world");
+  return -1;
+}
