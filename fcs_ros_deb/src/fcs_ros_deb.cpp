e965dac32e792dfa6a008a1956d64b58acd00e00
Bill Fan <1472451850@qq.com>
2020-11-25 22:45:20 -0500

    All: Updated cmake and package.xml to correctly depend on message package

--- fcs_ros_deb/src/fcs_ros_deb.cpp
+++ fcs_ros_deb/src/fcs_ros_deb.cpp
@@ -5,6 +5,7 @@
 #include <ros/ros.h>
 
 #include <fcs_ros_deb/fcs_ros_deb.hpp>
+//#include <msgs_ros_deb/FoilboatControl.h>
 
 int main(int argc, char** argv)
 {
---
+++ pid_ros_deb/src/controller/test.cpp
@@ -0,0 +1,6 @@
+#include "ros/ros.h"
+#include <msgs_ros_deb/FoilboatControl.h>
+
+int main(int argc, char** argv){
+  return 0;
+}
